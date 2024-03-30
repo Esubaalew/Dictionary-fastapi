0387fd8486a0c754b8f724f6bca84d48c05b2433
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-30 10:21:32 +0300

    added deployement files

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from dictionary.britannica import (
     get_entries,
@@ -9,6 +10,15 @@ from dictionary.britannica import (
 )
 
 app = FastAPI()
+
+# Allow requests from all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/britannica/entries/{word}")
