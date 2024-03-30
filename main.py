c0569657b5811a4417e7680bf424d6c75ec2a56c
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-30 19:35:32 +0300

    add docs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_redoc_html, get_swagger_ui_html,
+)
+from fastapi.responses import RedirectResponse
 
 from dictionary.britannica import (
     get_entries,
@@ -9,7 +13,19 @@ from dictionary.britannica import (
     get_definitions
 )
 
-app = FastAPI()
+app = FastAPI(
+    title="Azeb's Dictionary API",
+    description="API for accessing Britannica Dictionary data.",
+    summary="Azeb's API: Explore the depths of the English language with comprehensive definitions, word of the "
+            "day, and more.",
+    contact={
+        "name": "Esubalew Chekol",
+        "url": "https://github.com/esubaalew",
+
+    },
+
+    version="1.0"
+)
 
 # Allow requests from all origins
 app.add_middleware(
@@ -23,34 +39,86 @@ app.add_middleware(
 
 @app.get("/britannica/entries/{word}")
 async def britannica_entries(word: str):
+    """
+    Retrieve entries for a given word from the Britannica Dictionary.
+
+    Args:
+        word (str): The word to retrieve entries for.
+
+    Returns:
+        dict: A dictionary containing entries for the given word.
+    """
     entries = get_entries(word)
     return {"entries": entries}
 
 
 @app.get("/britannica/total_entries/{word}")
 async def britannica_total_entries(word: str):
+    """
+    Retrieve the total number of entries for a given word from the Britannica Dictionary.
+
+    Args:
+        word (str): The word to retrieve total entries for.
+
+    Returns:
+        dict: A dictionary containing the total number of entries for the given word.
+    """
     total_entries = get_total_entries(word)
     return {"total_entries": total_entries}
 
 
 @app.get("/britannica/word_of_the_day")
 async def britannica_word_of_the_day():
+    """
+    Retrieve the word of the day from the Britannica Dictionary.
+
+    Returns:
+        dict: A dictionary containing the word of the day.
+    """
     word_of_the_day = get_word_of_the_day()
     return {"word_of_the_day": word_of_the_day}
 
 
 @app.get("/britannica/speeches/{word}")
 async def britannica_parts(word: str):
+    """
+    Retrieve the parts of speech for a given word from the Britannica Dictionary.
+
+    Args:
+        word (str): The word to retrieve parts of speech for.
+
+    Returns:
+        dict: A dictionary containing the parts of speech for the given word.
+    """
     parts = get_parts(word)
     return {"parts_of_speech": parts}
 
 
 @app.get("/britannica/definitions/{word}")
 async def britannica_definitions(word: str):
+    """
+    Retrieve the definitions and examples for a given word from the Britannica Dictionary.
+
+    Args:
+        word (str): The word to retrieve definitions and examples for.
+
+    Returns:
+        dict: A dictionary containing the definitions and examples for the given word.
+    """
     definitions = get_definitions(word)
     return {"definitions": definitions}
 
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url='/redoc')
+
+
+@app.get("/redoc", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="Azeb's Dictionary API    ")
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(openapi_url="/openapi.json", title="Azeb's Dictionary API")
